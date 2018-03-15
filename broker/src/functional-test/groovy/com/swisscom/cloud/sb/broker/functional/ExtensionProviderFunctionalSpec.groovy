b38ce79c75f5fc5debfea586b2d4a6309cbe22d4
Zohaib Khan <zohaib.khan@swisscom.com>
2018-03-15 11:49:26 +0100

    Fix dequeue bug and add getApi test

--- broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/ExtensionProviderFunctionalSpec.groovy
+++ broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/ExtensionProviderFunctionalSpec.groovy
@@ -4,6 +4,7 @@ import com.swisscom.cloud.sb.broker.services.common.ServiceProviderLookup
 import com.swisscom.cloud.sb.broker.util.test.DummyExtension.DummyExtensionsServiceProvider
 import com.swisscom.cloud.sb.client.model.ProvisionResponseDto
 import org.springframework.http.ResponseEntity
+import org.yaml.snakeyaml.Yaml
 
 class ExtensionProviderFunctionalSpec extends BaseFunctionalSpec{
 
@@ -20,6 +21,15 @@ class ExtensionProviderFunctionalSpec extends BaseFunctionalSpec{
         ResponseEntity<ProvisionResponseDto> res = serviceLifeCycler.provision(false, null, null)
         then:
         "DummyExtensionURL" == res.body.extension_apis[0].discovery_url
+    }
+
+    def "Get api docs"(){
+        when:
+        String res = serviceBrokerClient.getApi(serviceLifeCycler.serviceInstanceId).body
+        Yaml parser = new Yaml()
+        parser.load(res)
+        then:
+        noExceptionThrown()
     }
 
     def "Execute async extension"(){
